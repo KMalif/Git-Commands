810a98742dd66fe95b2b23158f9e565060374f56
Khafidul Mualif <56641689+KMalif@users.noreply.github.com>
2024-01-08 15:32:13 +0700

    Update challenge-day1.js

--- challenge-day1.js
+++ challenge-day1.js
@@ -8,11 +8,27 @@ const bandingkanAngka = (angka1, angka2)=>{
   }
 }
 
+const balikKata = (kata) =>{
+  for (let i = kata.length-1; i >=0 ; i--){
+    console.log(kata[i]);
+  }
+}
 
 const konversiMenit = (detik)=>{
   const mnt = Math.floor(detik/60);
   const scd = detik%60;
   console.log(mnt + ':' + scd);
 }
+
+
+const xo = (str) =>{
+  
+}
+
+const dataHandling = (params) =>{
+  
+}
+
+
 
 
