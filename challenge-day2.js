cec78bfab6e1e3b64584b9e64f1f510c6ac46348
KMalif <khafidulmualif2019@gmail.com>
2024-01-09 12:04:38 +0700

    modify challenge 2

--- challenge-day2.js
+++ challenge-day2.js
@@ -6,7 +6,7 @@ const palindrome=(kata) => {
     return kata === newStr ? true : false
   }
 
-console.log(palindrome('katak'));
+// console.log(palindrome('katak'));
 
 const hitungJumlahKata = (kalimat) =>{
     let newArr = [];
@@ -26,39 +26,64 @@ const hitungJumlahKata = (kalimat) =>{
     return newArr.length;
 }
 
-console.log(hitungJumlahKata('khafidul mualif kasdkjla'));
+// console.log(hitungJumlahKata('khafidul mualif kasdkjla'));
 
 
 const changeVocals = (str) =>{
-    const vocals = ['a, i , u, e, o'];
-    const change = ['b, j, v, f, p'];
+    const vocals = ['a', 'i' , 'u', 'e', 'o', 'A', 'I', 'U', 'E','O' ];
+    const change = ['b', 'j', 'v', 'f', 'p', 'B', 'J', 'V', 'F', 'P'];
     let newStr = '';
     const splitStr = [];
 
-    for (let i = 0 ; i <= str.length -1 ; i++){
-            
+    for(let i = 0 ; i <= str.length-1 ; i++){
+        splitStr.push(str[i]);
     }
 
-
-
-    for (let i = 0 ; i <= str.length -1 ; i++){
-        
-    }
-}
-
-const reverseWord = (str) =>{
-    let newStr= '';
-    for(let i = str.length-1 ; i >= 0; i-- ){
-        newStr += str[i];
+    for (let i = 0; i < splitStr.length ; i++){
+        for(let j = 0; j < vocals.length; j++){
+            if(vocals[j] == splitStr[i]){
+                newStr += change[j];
+            }
+        }
+        if(newStr.length < i + 1){
+            newStr += splitStr[i];
+        }
     }
     return newStr;
 }
 
-// console.log(reverseWord('Alif '))
+console.log(changeVocals('aiuEo'));
 
-const setLowerUpperCase = (str) =>{
 
+String.prototype.reverseWord = function(){
+    let newStr= '';
+    for(let i = this.length-1 ; i >= 0; i-- ){
+        newStr += this[i];
+    }
+    return newStr;
 }
+
+const value = 'Alif'
+console.log(value.reverseWord())
+
+String.prototype.setLowerUpperCase = function() {
+    let splitStr = []
+    let newStr = ''
+    for(let i = 0 ; i <= this.length-1 ; i++){
+        splitStr.push(this[i]);
+    }
+
+    for(let i = 0 ; i <= splitStr.length-1 ; i++){
+        const element = splitStr[i];
+        if(element === element.toUpperCase()){
+            newStr += splitStr[i].toLowerCase()
+        }else if(element === element.toLowerCase()){
+            newStr += element.toUpperCase()
+        }
+    }
+    return newStr;
+}
+
 
 const removeSpaces = (str) =>{
     let newStr = '';
@@ -71,9 +96,9 @@ const removeSpaces = (str) =>{
 }
 
 const passwordGenerator = (str) =>{
+       
     
 }
 
-// console.log(removeSpaces(' Khafidul Mualif '));
 
 // console.log(passwordGenerator('Alif'))
